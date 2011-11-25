00c6ce697224cfa850b716c3c5d466b04174eecf
Nick Sutterer <apotonick@gmail.com>
2011-11-25 21:58:10 +0100

    added a README. contribute!

---
+++ README.mk
@@ -0,0 +1,27 @@
+h1. Webmachine Loves Roar
+
+_...and Roar Loves Webmachine!_
+
+
+h2. Introduction
+
+This is a sample application to show how [webmachine](https://github.com/seancribbs/webmachine-ruby) and [roar](https://github.com/apotonick/roar) can work together to build real RESTful systems in Ruby.
+
+
+h2. Usage
+
+To run the server:
+
+  $ ruby server.rb
+
+And, surprisingly, to run the client:
+
+  $ ruby client.rb
+
+
+h2. Contributors
+
+This project is currently done by
+
+* [Sean Cribbs](https://github.com/seancribbs)
+* [Nick Sutterer](https://github.com/apotonick)
